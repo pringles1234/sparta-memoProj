505fb16866e97daf79f2551c586579fbac6aec8e
Yujin Lee <dbwls9040@gmail.com>
2024-08-12 02:45:17 +0900

    create, read 구현

---
+++ src/main/java/com/sparta/memo/dto/MemoRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.memo.dto;
+
+import com.sparta.memo.entity.Memo;
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+
+
+}
---
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,21 @@
+package com.sparta.memo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Memo {
+
+    private Long id;
+    private String username;
+    private String contents;
+    private String etc;
+
+    public Memo(String username, String contents) {
+        this.username = username;
+        this.contents = contents;
+    }
+}
